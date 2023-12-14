a2e3f60d3bf0349bcb70ccfb7684ba111b4fff8e
MattCG <mattcontrerasg@gmail.com>
2023-12-14 06:24:39 -0300

    modulo tareas listo

--- preentrega2.js
+++ preentrega2.js
@@ -2,58 +2,6 @@ console.log(
   ` inicializacion de la aplicación 
      ============================================================ `
 );
-
-// Defino Variables globales
-let menuPpalState = true;
-let menuAgState;
-let menuTareasState;
-let menuInsumosState;
-
-// =================================================================
-
-// Defino funciones globales
-
-function generarLegajo(agentes) {
-  let legajo;
-  while (!agentes.some((agente) => agente.legajo === legajo)) {
-    legajo = Math.floor(Math.random() * 9000) + 1000;
-  }
-  return legajo;
-}
-
-// =================================================================
-
-// Defino clases
-
-class Agente {
-  constructor(nombre, posicion, sector) {
-    this.nombre = nombre;
-    this.posicion = posicion;
-    this.sector = sector;
-    this.legajo = generarLegajo(agentes);
-  }
-}
-
-class Tarea {
-  constructor(tipo, prioridad, descripcion) {
-    this.tipo = tipo;
-    this.prioridad = prioridad;
-    this.descripcion = descripcion;
-    this.estado = null;
-    this.creacion = null;
-  }
-}
-
-class Insumo {
-  constructor(nombreInsumo, tipoInsumo, stock) {
-    this.nombreInsumo = nombreInsumo;
-    this.tipoInsumo = tipoInsumo;
-    this.stock = stock;
-  }
-}
-
-// =================================================================
-
 // Arrays de Ejemplo iniciales
 
 let agentes = [
@@ -120,78 +68,110 @@ let agentes = [
 ];
 let tareas = [
   {
+    id: 1,
     tipo: "Fabricación",
     prioridad: "Baja",
     descripcion: "REVISAR Y AJUSTAR MAQUINARIA",
+    estado: "Pendiente",
+    creacion: new Date("2023-12-01T08:00:00"),
   },
   {
+    id: 2,
     tipo: "Reposición",
     prioridad: "Media",
     descripcion: "REPONER PRODUCTOS EN ESTANTERÍAS",
+    estado: "Pendiente",
+    creacion: new Date("2023-12-02T10:30:00"),
   },
   {
+    id: 3,
     tipo: "Ventas",
     prioridad: "Alta",
     descripcion: "ENTREGAR PEDIDO AL CLIENTE",
+    estado: "Pendiente",
+    creacion: new Date("2023-12-03T14:45:00"),
   },
   {
-    tipo: "Fabricación",
-    prioridad: "Urgente",
-    descripcion: "LIMPIAR ÁREA DE PRODUCCIÓN CON URGENCIA",
-  },
-  {
-    tipo: "Fabricación",
-    prioridad: "Baja",
-    descripcion: "REALIZAR INSPECCIÓN DE PRODUCTOS ACABADOS",
-  },
-  {
-    tipo: "Reparación",
-    prioridad: "Media",
-    descripcion: "REPARAR EQUIPO DE OFICINA",
-  },
-  {
-    tipo: "Ventas",
-    prioridad: "Alta",
-    descripcion: "ATENDER A CLIENTES EN MOSTRADOR",
-  },
-  {
+    id: 4,
     tipo: "Facturación",
     prioridad: "Urgente",
-    descripcion: "EMITIR FACTURAS CON URGENCIA",
-  },
-  {
-    tipo: "Resposicion",
-    prioridad: "Baja",
-    descripcion: "VERIFICAR Y REPONER STOCK EN ALMACÉN",
-  },
-  {
-    tipo: "Fabricación",
-    prioridad: "Media",
-    descripcion: "REALIZAR MANTENIMIENTO PREVENTIVO DE EQUIPOS",
+    descripcion: "EMITIR FACTURAS",
+    estado: "Pendiente",
+    creacion: new Date("2023-12-08T12:45:00"),
   },
 ];
+let tareasCompletadas = [];
 let insumos = [
   { nombreInsumo: "REACTIVOS DE PH", tipoInsumo: "Fabricación", stock: 500 },
   { nombreInsumo: "SILICONA", tipoInsumo: "Materias Primas", stock: 1000 },
   { nombreInsumo: "DETERGENTE", tipoInsumo: "Materias Primas", stock: 750 },
   { nombreInsumo: "PAPEL TOALLA", tipoInsumo: "Administrativo", stock: 300 },
   { nombreInsumo: "EMULSIONANTE", tipoInsumo: "Materias Primas", stock: 200 },
-  { nombreInsumo: "CERA", tipoInsumo: "Materias Primas", stock: 400 },
+  { nombreInsumo: "CERA", tipoInsumo: "Materias Primas", stock: 0 },
   { nombreInsumo: "GUANTES", tipoInsumo: "Fabricación", stock: 600 },
   { nombreInsumo: "FILTROS", tipoInsumo: "Fabricación", stock: 450 },
-  { nombreInsumo: "CEPILLOS", tipoInsumo: "Fabricación", stock: 350 },
+  { nombreInsumo: "CEPILLOS", tipoInsumo: "Fabricación", stock: 0 },
   { nombreInsumo: "ALCOHOL", tipoInsumo: "Materias Primas", stock: 300 },
-  {
-    nombreInsumo: "PAPEL DE OFICINA",
-    tipoInsumo: "Administrativo",
-    stock: 500,
-  },
+  { nombreInsumo: "PAPEL DE OFICINA", tipoInsumo: "Administrativo", stock: 500,},
   { nombreInsumo: "BOLÍGRAFOS", tipoInsumo: "Administrativo", stock: 200 },
   { nombreInsumo: "CINTA ADHESIVA", tipoInsumo: "Administrativo", stock: 150 },
 ];
 
 // =================================================================
+// Defino Variables globales
+let menuPpalState = true;
+let menuAgState;
+let menuTareasState;
+let menuInsumosState;
+let tareaID = 5; // <- la empiezo desde 5 por el ultimo ID del array de ejemplo. Sino iniciaria en 1
 
+// =================================================================
+
+// Defino clases
+
+class Agente {
+  constructor(nombre, posicion, sector) {
+    this.nombre = nombre;
+    this.posicion = posicion;
+    this.sector = sector;
+    this.legajo = generarLegajo(agentes);
+  }
+}
+
+class Tarea {
+  constructor(tipo, prioridad, descripcion) {
+    this.tipo = tipo;
+    this.prioridad = prioridad;
+    this.descripcion = descripcion;
+    this.estado = "Pendiente";
+    this.creacion = new Date();
+    this.id = tareaID;
+  }
+}
+
+class Insumo {
+  constructor(nombreInsumo, tipoInsumo, stock) {
+    this.nombreInsumo = nombreInsumo;
+    this.tipoInsumo = tipoInsumo;
+    this.stock = stock;
+  }
+}
+
+// =================================================================
+
+// Defino funciones globales
+
+const generarLegajo = (agentes) => {
+  let legajo;
+
+  do {
+    legajo = Math.floor(Math.random() * 9000) + 1000;
+  } while (agentes.some((agente) => agente.legajo === legajo));
+
+  return legajo;
+};
+
+// =================================================================
 // ciclo menu principal
 
 const menuInicio = () => {
@@ -236,10 +216,9 @@ const menuAgentes = () => {
       prompt(` Administrar nómina de empleados.
       
         1- Ver nómina
-        2- Administrar agente
-        3- Alta nuevo agente  
-        4- Baja agente      
-        5- Volver
+        2- Alta nuevo agente  
+        3- Baja agente      
+        4- Volver
         `)
     );
     switch (seleccionAg) {
@@ -247,15 +226,12 @@ const menuAgentes = () => {
         verNomina();
         break;
       case 2:
-        administrarAgente();
-        break;
-      case 3:
         altaAgente();
         break;
-      case 4:
+      case 3:
         bajaAgente();
         break;
-      case 5:
+      case 4:
         menuInicio();
         menuAgState = false;
         break;
@@ -357,11 +333,189 @@ const verNomina = () => {
   }
 };
 
-const administrarAgente = () => {};
+const altaAgente = () => {
+  let nombre;
+  let posicion;
+  let sector;
+  let selecPosicion;
+  let selecSector;
 
-const altaAgente = () => {};
+  do {
+    nombre = prompt("Indique nombre y apellido del nuevo agente").toUpperCase();
+  } while (!isNaN(nombre) || nombre === null);
 
-const bajaAgente = () => {};
+  while (
+    isNaN(selecPosicion) ||
+    selecPosicion === null ||
+    selecPosicion < 1 ||
+    selecPosicion > 3
+  ) {
+    selecPosicion = parseInt(
+      prompt(`Seleccione el puesto en la empresa para ${nombre}.
+
+1- Operador
+2- Encargado
+3- Supervisor
+`)
+    );
+    switch (selecPosicion) {
+      case 1:
+        posicion = "Operador";
+        break;
+      case 2:
+        posicion = "Encargado";
+        break;
+      case 3:
+        posicion = "Supervisor";
+        break;
+      default:
+        alert("Opción inexistente");
+        break;
+    }
+  }
+
+  while (
+    isNaN(selecSector) ||
+    selecSector === null ||
+    selecSector < 1 ||
+    selecSector > 4
+  ) {
+    selecSector = parseInt(
+      prompt(`Seleccione el sector de en la empresa al que ingresa el ${posicion}: ${nombre}.
+
+  1- Facturación
+  2- Ventas
+  3- Fabricacion
+  4- Reposición
+  `)
+    );
+    switch (selecSector) {
+      case 1:
+        sector = "Facturación";
+        break;
+      case 2:
+        sector = "Ventas";
+        break;
+      case 3:
+        sector = "Fabricación";
+        break;
+      case 4:
+        sector = "Reposición";
+        break;
+      default:
+        alert("Opción inexistente");
+        break;
+    }
+  }
+  let agente = new Agente(nombre, posicion, sector);
+  agentes.push(agente);
+
+  alert(` Se ha ingresado al nuevo agente a la base de datos:
+
+========================
+Puesto: ${agente.posicion}
+Nombre: ${agente.nombre} 
+Sector: ${agente.sector}  
+Legajo: ${agente.legajo}
+=========================
+ `);
+};
+
+const bajaAgente = () => {
+  let confirmar = false;
+  let agenteEliminado = prompt(
+    "Ingrese el NOMBRE o el numero de LEGAJO del agente que desea dar de baja"
+  );
+
+  if (
+    !isNaN(agenteEliminado) &&
+    agentes.some((agente) => agente.legajo === parseInt(agenteEliminado))
+  ) {
+    while (!confirmar) {
+      const agentePorLegajo = agentes.find(
+        (agente) => agente.legajo === parseInt(agenteEliminado)
+      );
+
+      let confirmacion = parseInt(
+        prompt(`¿Esta seguro de que desea eliminar al siguiente agente?
+
+        ===============
+         Puesto: ${agentePorLegajo.posicion}
+         Nombre: ${agentePorLegajo.nombre}
+         Sector: ${agentePorLegajo.sector}
+         Legajo: ${agentePorLegajo.legajo}
+         ===============
+      1. SI
+      2. NO`)
+      );
+
+      switch (confirmacion) {
+        case 1:
+          alert(
+            `Se eliminó el agente: ${agentePorLegajo.nombre} del sector ${agentePorLegajo.sector}`
+          );
+          agentes = agentes.filter(
+            (agente) => agente.legajo !== parseInt(agenteEliminado)
+          );
+          confirmar = true;
+          break;
+        case 2:
+          alert("Baja Cancelada");
+          confirmar = true;
+          break;
+        default:
+          alert("Opcion inválida");
+          break;
+      }
+    }
+  } else if (
+    isNaN(agenteEliminado) &&
+    agentes.some((agente) => agente.nombre === agenteEliminado.toUpperCase())
+  ) {
+    while (!confirmar) {
+      const agentePorNombre = agentes.find(
+        (agente) => agente.nombre === agenteEliminado.toUpperCase()
+      );
+
+      let confirmacion = parseInt(
+        prompt(`¿Esta seguro de que desea eliminar al siguiente agente?
+
+        ===============
+         Puesto: ${agentePorNombre.posicion}
+         Nombre: ${agentePorNombre.nombre}
+         Sector: ${agentePorNombre.sector}
+         Legajo: ${agentePorNombre.legajo}
+        ===============
+
+      1. SI
+      2. NO`)
+      );
+
+      switch (confirmacion) {
+        case 1:
+          alert(
+            `Se eliminó el agente: ${agentePorNombre.nombre} del sector ${agentePorNombre.sector}`
+          );
+          agentes = agentes.filter(
+            (agente) => agente.nombre !== agenteEliminado.toUpperCase()
+          );
+          confirmar = true;
+          break;
+        case 2:
+          alert("Baja Cancelada");
+          confirmar = true;
+          break;
+        default:
+          alert("Opcion inválida");
+          break;
+      }
+    }
+  } else {
+    alert("No se encontró ningún agente con el legajo o nombre proporcionado.");
+  }
+};
+
+// =========================================================================
 
 // Ciclo menu modulo tareas
 
@@ -369,29 +523,25 @@ const menuTareas = () => {
   menuTareasState = true;
   while (menuTareasState) {
     let seleccionTarea = parseInt(
-      prompt(` Nómina de Empleados de Quimishop SRL.
+      prompt(` Lista de tareas pendientes.
 
-        1- Ver Nómina
-        2- Administrar Agente
-        3- Alta Nuevo Agente  
-        4- Baja Agente      
-        5- Salir
+        1- Ver Tareas
+        2- Agregar Tareas Nuevas
+        3- Marcar Tarea Realizada        
+        4- Salir
         `)
     );
     switch (seleccionTarea) {
       case 1:
-        verNomina();
+        verTareas();
         break;
       case 2:
-        administrarAgente();
+        agregarTarea();
         break;
       case 3:
-        altaAgente();
+        tareaRealizada();
         break;
       case 4:
-        bajaAgente();
-        break;
-      case 5:
         menuInicio();
         menuTareasState = false;
         break;
@@ -401,6 +551,190 @@ const menuTareas = () => {
     }
   }
 };
+
+// Funciones Menu Tareas
+const verTareas = () => {
+  let listaTareas = tareas.map(
+    (tarea) =>
+      `ID:${tarea.id} - ${tarea.descripcion} en ${tarea.tipo} - con prioridad: ${tarea.prioridad} - ${tarea.estado}`
+  );
+    if (listaTareas.length > 0) {
+    alert(listaTareas.join("\n ------- \n"));
+  } else {
+    alert("Sin Tareas Pendientes");
+  }
+
+  if (tareasCompletadas.length > 0) {
+    let listaCompletadas = tareasCompletadas.map((tarea) => `ID:${tarea.id} - ${tarea.descripcion} en ${tarea.tipo} - ${tarea.estado}`);
+    alert(`Tareas Completadas:
+    ==================
+    ${listaCompletadas.join("\n ------- \n")}`);
+  };
+};
+
+const agregarTarea = () => {
+  let descripcion;
+  let tipoTarea;
+  let prioridad;
+  let selecTipo;
+  let selecPrio;
+
+  do {
+    descripcion = prompt(
+      "Indique brevemente la descripción de la nueva tarea"
+    ).toUpperCase();
+  } while (!isNaN(descripcion) || descripcion === null);
+
+  while (
+    isNaN(selecTipo) ||
+    selecTipo === null ||
+    selecTipo < 1 ||
+    selecTipo > 4
+  ) {
+    selecTipo = parseInt(
+      prompt(`Seleccione el tipo de tarea para su clasificación:
+
+1- Fabricación
+2- Reposición
+3- Ventas
+4- Facturación
+`)
+    );
+    switch (selecTipo) {
+      case 1:
+        tipoTarea = "Fabricación";
+        break;
+      case 2:
+        tipoTarea = "Reposición";
+        break;
+      case 3:
+        tipoTarea = "Ventas";
+        break;
+      case 4:
+        tipoTarea = "Facturación";
+        break;
+      default:
+        alert("Opción inexistente");
+        break;
+    }
+  }
+
+  while (
+    isNaN(selecPrio) ||
+    selecPrio === null ||
+    selecPrio < 1 ||
+    selecPrio > 4
+  ) {
+    selecPrio = parseInt(
+      prompt(`Indique el nivel de prioridad de la nueva tarea.
+
+  1- Baja
+  2- Media
+  3- Alta
+  4- Urgente
+  `)
+    );
+    switch (selecPrio) {
+      case 1:
+        prioridad = "Baja";
+        break;
+      case 2:
+        prioridad = "Media";
+        break;
+      case 3:
+        prioridad = "Alta";
+        break;
+      case 4:
+        prioridad = "Urgente";
+        break;
+      default:
+        alert("Opción inexistente");
+        break;
+    }
+  }
+  let tarea = new Tarea(descripcion, tipoTarea, prioridad);
+  tareas.push(tarea);
+  tareaID++;
+
+  alert(` Se ha ingresado la nueva tarea a la lista de pendientes:
+
+========================
+ID: ${tarea.id}
+Descripción: ${tarea.descripcion}
+Tipo: ${tarea.nombre} 
+Prioridad: ${tarea.prioridad}  
+Fecha: ${tarea.creacion}
+=========================
+ `);
+};
+
+const tareaRealizada = () => {
+
+let confirmarTarea = false;
+let tareaBuscada = parseInt(prompt(`Ingrese el ID de la tarea que desea revisar`));
+
+if (
+  !isNaN(tareaBuscada) &&
+  tareas.some((tarea) => tarea.id === tareaBuscada)
+) {
+  while (!confirmarTarea) {
+const marcarTarea = tareas.find((tarea) => tarea.id === tareaBuscada);
+
+
+let opcion = parseInt(prompt(`
+Desea marcar la tarea como completada y eliminarla de la lista de pendientes?
+1- Si
+2- No
+--------------
+ID: ${marcarTarea.id}
+Descripción: ${marcarTarea.descripcion}
+Tipo: ${marcarTarea.tipo}
+Prioridad: ${marcarTarea.prioridad}
+Fecha de Creación: ${marcarTarea.creacion}
+Estado: ${marcarTarea.estado}
+`));
+
+switch (opcion) {
+  case 1: 
+  marcarTarea.estado = "Completada";
+  alert(`La tarea:
+  ----------------
+ID: ${marcarTarea.id}
+Descripción: ${marcarTarea.descripcion}
+Tipo: ${marcarTarea.tipo}
+Prioridad: ${marcarTarea.prioridad}
+Fecha de Creación: ${marcarTarea.creacion}
+Estado: ${marcarTarea.estado}
+  ----------------
+  se ha eliminado de la lista de pendientes.`);
+
+  tareasCompletadas.push(marcarTarea);
+  tareas = tareas.filter((tarea) => tarea.id !== marcarTarea.id);
+  confirmarTarea = true;
+  break;
+  case 2: 
+  alert(`La tarea:
+  ----------------
+ID: ${marcarTarea.id}
+Descripción: ${marcarTarea.descripcion}
+Tipo: ${marcarTarea.tipo}
+Prioridad: ${marcarTarea.prioridad}
+Fecha de Creación: ${marcarTarea.creacion}
+Estado: ${marcarTarea.estado}
+  ----------------
+  permanecerá en la lista de pendientes.`);
+  confirmarTarea = true;
+  break;
+  default:
+    alert("Opcion invalida")
+    break;
+};
+};
+};
+};
+
+
+// =================================================================================
 
 // Ciclo menu modulo insumos
 
@@ -414,7 +748,7 @@ const menuInsumos = () => {
         2- Administrar Agente
         3- Alta Nuevo Agente  
         4- Baja Agente      
-        5- Salir
+        5- Volver
         `)
     );
     switch (seleccionInsumos) {
